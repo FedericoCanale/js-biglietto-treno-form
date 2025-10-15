5e3bab02478b7485017cdc6e538e6c20bd0c6fcb
FedericoCanale <icocanale@gmail.com>
2025-10-15 13:25:30 +0200

    add to do list and tools for Milestone 1

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/* To Do List
+    1. 	Creare due file: index.html e main.js.
+	2.	Creare in HTML due input e un bottone.
+	4.	In JS recuperare i valori e calcolare il prezzo base (km × 0.21).
+	5.	Applicare:
+        •	sconto 20% se eta < 18
+        •	sconto 40% se eta > 65
+        •	nessuno sconto se eta è tra 18 e 65
+	6.	Stampare il risultato in console.
+    Tools
+    •	getElementById()
+	•	addEventListener()
+	•	Number()
+	•	if / else if / else
+	•	console.log()
+	•	.toFixed(2)
+    */
