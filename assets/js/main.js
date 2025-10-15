ab90c1b19646c2fa14013203d02614539a786b3a
FedericoCanale <icocanale@gmail.com>
2025-10-15 14:22:10 +0200

    add eventlistener and conditions for discounts (milestone 1 complete)

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,19 +17,31 @@
     */
 
 // Prezzo per km
-
+const priceKm = 0.21;
 
 // Prendo il bottone
-
+const btn = document.getElementById('countBtn');
 // Aggiungo evento click
+btn.addEventListener('click', function() {
+    // Recupero i valori dagli input
+    const km = Number(document.getElementById('km').value);
+    const age = Number(document.getElementById('age').value);
+ 
+    // Calcolo prezzo base
+  let priceTotal = km * priceKm;
 
-// Recupero i valori dagli input
-  
-// Calcolo prezzo base
-  
 
-// Applico eventuali sconti
-  
+    // Applico eventuali sconti
+  if (age < 18) {
+    priceTotal *= 0.8; // sconto 20%
+    console.log("Applicato sconto del 20% per minorenni.");
+  } else if (age > 65) {
+    priceTotal *= 0.6; // sconto 40%
+    console.log("Applicato sconto del 40% per over 65.");
+  } else {
+    console.log("Nessuno sconto applicato (fascia 18-65 anni).");
+  }
 
 // Mostro il risultato finale in console
-  
+  console.log(`Il prezzo totale del biglietto è: €${priceTotal.toFixed(2)}`);
+});
